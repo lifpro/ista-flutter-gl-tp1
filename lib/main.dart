bfeb28a288a6180fbd9720fd9b96f4b0b4797a53
«Lif» <ysalissou@gmail.com>
2024-01-09 18:58:49 +0000

    image

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,48 @@ void main() {
         body: const Center(
           child: Text('Salut'),
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Center(
+                    child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/logo.png',
+                      height: 50,
+                    ),
+                    const Text(
+                      'Gmail',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 34,
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+              const ListTile(
+                title: Text('Reseaux Sociaux'),
+                leading: Icon(Icons.person_3_rounded),
+                trailing: Text('5 Nouv'),
+              ),
+              const ListTile(
+                title: Text('Reseaux Sociaux'),
+                leading: Icon(Icons.person_3_rounded),
+                trailing: Text('5 Nouv'),
+              ),
+              Container(),
+            ],
+          ),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(label: 'Accueil', icon: Icon(Icons.home)),
             BottomNavigationBarItem(label: 'Profil', icon: Icon(Icons.person)),
           ],
-          backgroundColor: Colors.greenAccent,
+          backgroundColor: Colors.blue,
         ),
       ),
       debugShowCheckedModeBanner: false,
